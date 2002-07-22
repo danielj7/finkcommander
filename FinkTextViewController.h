340147dc259cae422644ec26749de5c89a5f0939
Steven J. Burr <sburrious@users.sourceforge.net>
2002-07-22 01:09:39 +0000

    Added optional scrollback buffer limit

---
+++ FinkTextViewController.h
@@ -0,0 +1,42 @@
+/*
+File: FinkTextViewController.h
+
+ FinkTextViewController's sole raison d'etre (at the time of this writing) is
+ to implement a limited scrollback buffer for the fink command output
+ text displayed by FinkCommander.  A subclass rather than a category
+ was used in order to track the number of calls to the appendString
+ method with an instance variable.
+
+ Copyright (C) 2002  Steven J. Burr
+
+ This program is free software; you may redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ Contact the author at sburrious@users.sourceforge.net.
+*/
+ 
+#import <Cocoa/Cocoa.h>
+#import "FinkGlobals.h"
+
+@interface FinkTextViewController : NSTextView
+{
+	NSUserDefaults *defaults;
+	int lines;
+	int bufferLimit;
+}
+
+-(void)setString:(NSString *)aString;
+-(void)appendString:(NSString *)s;
+
+@end
---
+++ FinkTextViewController.m
@@ -0,0 +1,74 @@
+/*
+File: FinkTextViewController.m
+
+ See the header file, FinkTextViewController.h, for interface and license information.
+
+*/
+
+#import "FinkTextViewController.h"
+
+@implementation FinkTextViewController
+
+//override parent method
+-(void)setString:(NSString *)aString
+{
+	lines = 0;
+	bufferLimit = [[NSUserDefaults standardUserDefaults] integerForKey:FinkBufferLimit];
+	[super setString:aString];
+}
+
+-(int)numberOfLinesInString:(NSString *)s
+{
+	NSScanner *lineScanner = [NSScanner scannerWithString:s];
+	NSCharacterSet *returnSet = [NSCharacterSet characterSetWithCharactersInString: @"\n"];
+	int numlines = 0;
+	
+	while (! [lineScanner isAtEnd]){
+		[lineScanner scanUpToCharactersFromSet:returnSet intoString:NULL];
+		[lineScanner scanString:@"\n" intoString:NULL];
+		numlines++;
+	}
+	return numlines;
+}
+
+-(NSRange)rangeOfLinesAtTopOfView:(int)numlines
+{
+	NSString *viewString = [self string];
+	int i, test;
+	int lastReturn = 0;
+	
+	for (i = 0; i < numlines; i++){
+		test = [viewString rangeOfString:@"\n"
+					options:0
+					range:NSMakeRange(lastReturn + 1, [viewString length] - lastReturn - 1)].location;
+		if (DEBUGGING) { NSLog(@"test: %d, lastReturn: %d", test, lastReturn); }
+		if (test == NSNotFound) break;
+		lastReturn = test;
+	}
+	return NSMakeRange(0, lastReturn);
+}
+
+
+-(void)appendString:(NSString *)s
+{
+	if (bufferLimit > 0){
+		int overflow;
+		NSRange r;
+			
+		lines += [self numberOfLinesInString:s];
+		overflow = lines - bufferLimit;
+		if (overflow > 10){
+			r = [self rangeOfLinesAtTopOfView:overflow];		
+			if (DEBUGGING) { 
+				NSLog(@"Deleting characters 0 to %d", lines, r.length - 1); 
+			} 
+			[[self textStorage] deleteCharactersInRange:r];
+			lines = 0;
+		}
+	}
+	
+	[[self textStorage] appendAttributedString:
+		[[[NSAttributedString alloc] initWithString: s] autorelease]];
+}
+
+@end
