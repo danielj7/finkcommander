facf40612f37f62419843a72f3893cb8835ee29e
Steven J. Burr <sburrious@users.sourceforge.net>
2003-01-03 17:25:09 +0000

    Fixed missing files, directory structure

---
+++ SBTreeFiles/SBBrowserCell.h
@@ -0,0 +1,8 @@
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SBBrowserCell : NSBrowserCell {
+}
+
+@end
---
+++ SBTreeFiles/SBBrowserCell.m
@@ -0,0 +1,18 @@
+
+
+#import "SBBrowserCell.h"
+
+
+@implementation SBBrowserCell
+
++(NSImage *)branchImage
+{
+	return nil;
+}
+
++(NSImage *)highlightedBranchImage
+{
+	return nil;
+}
+
+@end
